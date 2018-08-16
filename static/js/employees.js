c7c64e569ca76133ca30cc596813875d75f45a72
Jeff <jlynch6694@yahoo.com>
2018-08-16 11:49:06 -0400

    weather app

--- static/js/employees.js
+++ static/js/employees.js
@@ -2,3 +2,16 @@ setTimeout(function() {
 $("#home_link").removeClass("active");
 $("#emp_link").addClass("active");
 }, 5);
+
+$.getJSON("../../assets/employees.json",
+function (data) {
+    var tr;
+    for (var i = 0; i < data.length; i++) {
+        tr = $('<tr/>');
+        tr.append("<td>" + data[i].name+ "</td>");
+        tr.append("<td>" + data[i].salary + "</td>");
+        tr.append("<td>" + data[i].title + "</td>");
+        tr.append("<td>" + data[i].attendance + "</td>")
+        $('table').append(tr);
+    }
+});
